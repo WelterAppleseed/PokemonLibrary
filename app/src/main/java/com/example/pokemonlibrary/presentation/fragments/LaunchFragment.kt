155b24dfda286a4302049f435ba1025480b32094
WelterAppleseed <threedaystomars@yandex.ru>
2022-02-18 12:59:12 +0300

    Launch Commit

--- app/src/main/java/com/example/pokemonlibrary/presentation/fragments/LaunchFragment.kt
+++ app/src/main/java/com/example/pokemonlibrary/presentation/fragments/LaunchFragment.kt
@@ -1,41 +1,23 @@
 package com.example.pokemonlibrary.presentation.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.pokemonlibrary.PokemonLibraryApp
 import com.example.pokemonlibrary.R
-import com.example.pokemonlibrary.domain.SearchPokemonViewModel
-import com.example.pokemonlibrary.domain.RandomAndSearchSharedViewModel
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.pokemonlibrary.domain.RepositoryPokemonViewModel
+import com.example.pokemonlibrary.domain.SearchRecyclerViewModel
 import kotlinx.android.synthetic.main.fragment_launch.view.*
 import javax.inject.Inject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [LaunchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class LaunchFragment : BaseFragment() {
 
-    private lateinit var viewModelRandomAndSearch: RandomAndSearchSharedViewModel
+    private lateinit var viewModelRandomAndSearch: SearchRecyclerViewModel
 
-    var searchPokemonViewModel: SearchPokemonViewModel? = null
+    var repositoryPokemonViewModel: RepositoryPokemonViewModel? = null
     @Inject set
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,32 +31,27 @@ class LaunchFragment : BaseFragment() {
 
     }
     private fun initViewModel(view: View) {
-        viewModelRandomAndSearch = ViewModelProviders.of(requireActivity()).get(RandomAndSearchSharedViewModel::class.java)
-        searchPokemonViewModel?.getAllPokemon()
-        searchPokemonViewModel?.getLiveDataPokemon()?.observe(this, Observer {
-            viewModelRandomAndSearch.bundleFromRandomToSearch.value = it
-            view.progress_bar.animate().alpha(0F).setDuration(1000).withEndAction {
-                navigateTo(null, R.id.action_launchFragment_to_searchedPokemonsFragment)
-            }
-        })
-    }
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment LaunchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            LaunchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        if (!isOnline(this.context!!)) {
+            androidx.appcompat.app.AlertDialog.Builder(this.context!!)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Internet Connection Alert")
+                .setCancelable(false)
+                .setMessage("Please Check Your Internet Connection")
+                .setPositiveButton(
+                    "Close"
+                ) { _, _ ->
+                    activity?.finish()
+                }.show()
+        } else {
+            viewModelRandomAndSearch =
+                ViewModelProviders.of(requireActivity()).get(SearchRecyclerViewModel::class.java)
+            repositoryPokemonViewModel?.getAllPokemon()
+            repositoryPokemonViewModel?.getLiveDataPokemon()?.observe(this, {
+                viewModelRandomAndSearch.bundleFromSearch.value = it.toMutableList()
+                view.progress_bar.animate().alpha(0F).setDuration(1000).withEndAction {
+                    navigateTo(null, R.id.action_launchFragment_to_menuFragment)
                 }
-            }
+            })
+        }
     }
 }
