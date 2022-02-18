9d2acc172a5ee645e0ce176f36ea7158cda6b233
WelterAppleseed <threedaystomars@yandex.ru>
2022-02-18 12:07:13 +0300

    Menu Commit

--- app/src/main/java/com/example/pokemonlibrary/presentation/fragments/MenuFragment.kt
+++ app/src/main/java/com/example/pokemonlibrary/presentation/fragments/MenuFragment.kt
@@ -9,23 +9,27 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.pokemonlibrary.PokemonLibraryApp
 import com.example.pokemonlibrary.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_menu.view.*
 
 
 class MenuFragment : BaseFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initBackClickCallback(null, 0)
+        initMenuClickListener(R.id.action_favoritePokemonsFragment_to_menuFragment, R.id.action_menuFragment_to_favoritePokemonsFragment)
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view =  inflater.inflate(R.layout.fragment_menu, container, false)
+        view.to_random_fragment_b.setOnClickListener {
+            navigateTo(null, R.id.action_menuFragment_to_randomPokemonsFragment)
+        }
+        view.to_search_fragment_b.setOnClickListener {
+            navigateTo(null, R.id.action_menuFragment_to_searchedPokemonsFragment)
+        }
         return view
-    }
-
-    override fun onStart() {
-        super.onStart()
     }
 }
